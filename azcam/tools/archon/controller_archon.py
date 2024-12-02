cc225bded9a06604fef9a760d29d5dba3aecbb72
Michael Lesser <mlesser@arizona.edu>
2024-12-02 16:49:32 -0700

    update

--- azcam/tools/archon/controller_archon.py
+++ azcam/tools/archon/controller_archon.py
@@ -979,7 +979,7 @@ class ControllerArchon(Controller):
 
             for indx in range(cnt):
                 param = "PARAMETER" + str(indx)
-                print(param)
+                print(param, self.dict_config[param])
                 parname = self.dict_config[param].split("=")[0]
                 parvalue = self.dict_config[param].split("=")[1]
                 self.parameters[parname] = parvalue
